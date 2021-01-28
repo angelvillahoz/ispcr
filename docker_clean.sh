745b475baacf41230594cd4706ddbcafd345d049
Angel Villahoz-Baleta <avillaho@all-development-environment.novalocal>
2021-01-28 19:26:32 +0000

    Fourteenth version

--- docker_clean.sh
+++ docker_clean.sh
@@ -2,6 +2,5 @@ docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 docker rmi ispcr_ispcr_server
 docker rmi ispcr_php_server
-docker rmi mariadb:10.3
 docker rmi $(docker images -f "dangling=true" -q)
 docker volume prune -f
