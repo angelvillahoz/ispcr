5fab93297e27ba9d0c592f54d30251ffdfd146f8
Angel Villahoz-Baleta <avillaho@all-development-environment.novalocal>
2021-03-05 20:45:15 +0000

    Sixteenth version

--- docker_clean.sh
+++ docker_clean.sh
@@ -1,6 +1,11 @@
-docker stop $(docker ps --filter "name=ispcr_ispcr_server_1" --filter "name=ispcr_mariadb_server_1" --filter "name=ispcr_php_server_1" -q)
-docker rm ispcr_ispcr_server_1 ispcr_mariadb_server_1 ispcr_php_server_1
-docker rmi ispcr_ispcr_server
-docker rmi ispcr_php_server
-docker rmi $(docker images -f "dangling=true" -q)
+docker stop $(docker ps --filter "name=ispcr_ispcr_server_1" \
+    --filter "name=ispcr_mariadb_server_1" \
+    --filter "name=ispcr_php_server_1" \
+    -q)
+docker rm ispcr_ispcr_server_1 \
+    ispcr_mariadb_server_1 \
+    ispcr_php_server_1
+docker rmi ispcr_ispcr_server \
+    ispcr_php_server \
+    $(docker images -f "dangling=true" -q)
 docker volume prune -f
